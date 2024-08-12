b81374b45c89dbc11c16d2fd1b4531c6498a433b
anton <seminanton2@gmail.com>
2024-08-12 14:29:13 +0300

    добавил middleware AuthorizeMiddleware

---
+++ app/Http/Middleware/AuthorizeMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthorizeMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $queryParameters = $request->all();
+        info('query', $request->all());
+        if (key_exists('access', $queryParameters)
+            && $queryParameters['access'] === 'yes') {
+            return $next($request);
+        }
+        
+        return abort(403);
+    }
+}
