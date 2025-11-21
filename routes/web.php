0fd65f0dcf0bf631e05202efedb51181ccbf6fc9
anton <seminanton2@gmail.com>
2025-11-21 11:19:40 +0300

    Исправление

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::get('/', fn() => inertia('Page/Home'))->middleware('auth');
  */
 Route::get('/login/', [UserController::class, 'login'])->name('login');
 Route::post('/login/', [UserController::class, 'loginPost']);
-//Route::middleware('auth')->group(function () {
+Route::middleware('auth')->group(function () {
     Route::post('/logout/', [UserController::class, 'logout']);
     Route::resource('users', UserController::class)->scoped(['user' => 'username']);
-//})->where(['user' => '[a-zA-Z0-9]+']);
+})->where(['user' => '[a-zA-Z0-9]+']);
